bb41f8d93a4764cebbfb74372e962308a9600248
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-17 19:14:33 +0300

    Lesson 27 - Conditional rendering with the boolean AND operator.

--- src/UltimateMachine.js
+++ src/UltimateMachine.js
@@ -1,22 +1,45 @@
+// import { useState } from "react";
+
+// const ErrorMessage = () => <span>Oh noes you broke it!</span>;
+
+// const UltimateMachine = () => {
+//   const [showError, setShowError] = useState(false);
+
+//   const onClickHandler = () => {
+//     setShowError((i) => !i);
+//   };
+
+//   return (
+//     <section>
+//       <h1>The Ultimate Machine</h1>
+//       {showError && <ErrorMessage />}
+//       <button type="button" onClick={onClickHandler} aria-pressed={showError}>
+//         Toggle Error
+//       </button>
+//     </section>
+//   );
+// };
+
+// export default UltimateMachine;
+
 import { useState } from "react";
 
-const OnMessage = () => <span>The machine is ON!</span>;
-
-const OffMessage = () => <span>The machine is OFF!</span>;
+const ErrorMessage = ({ showError }) =>
+  showError ? <span>Oh noes you broke it!</span> : null;
 
 const UltimateMachine = () => {
-  const [isOn, setIsOn] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const onClickHandler = () => {
-    setIsOn((i) => !i);
+    setShowError((i) => !i);
   };
 
   return (
     <section>
       <h1>The Ultimate Machine</h1>
-      {isOn ? <OnMessage /> : <OffMessage />}
-      <button type="button" onClick={onClickHandler} aria-pressed={isOn}>
-        On/Off
+      <ErrorMessage showError={showError} />
+      <button type="button" onClick={onClickHandler} aria-pressed={showError}>
+        Toggle Error
       </button>
     </section>
   );
