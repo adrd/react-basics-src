0564fb2b78adefa20d7772e6e5d3fff14a910cd7
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-17 13:29:08 +0300

    Lesson 22 - Building re-usable Components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react";
 import Counter from "./Counter";
+import Random from "./Random";
 
-const App = () => <Counter />;
+const App = () => (
+  <Fragment>
+    <Counter />
+    <Random />
+  </Fragment>
+);
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,20 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import InteractiveView from "./InteractiveView";
 
-// CounterView = child component
-// child component tell a parent component to modify a value via events
-const CounterView = ({ counterValue, onIncrement }) => (
-  <Fragment>
-    <p>{counterValue}</p>
-    <button type="button" onClick={onIncrement}>
-      Increment
-    </button>
-  </Fragment>
-);
-
-// Counter = parent component
-// data are passed to child component (CounterView) via props
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   const onIncrementHandler = () => {
-    setCounter(counter + 1);
+    // setCounter(counter + 1);
+    setCounter((c) => c + 1);
   };
 
   return (
-    <CounterView counterValue={counter} onIncrement={onIncrementHandler} />
+    <InteractiveView
+      value={counter}
+      onAction={onIncrementHandler}
+      actionText="Increment"
+    />
   );
 };
 
