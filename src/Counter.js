0fab63a6391bda1b0093920203b3de92f8c66495
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-18 13:32:57 +0300

    Lesson 36 - Using the empty dependency Array in useEffects.

--- src/Counter.js
+++ src/Counter.js
@@ -2,22 +2,26 @@ import { Fragment, useState, useEffect } from "react";
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-  const [internalCount, setInternalCount] = useState(0);
 
   useEffect(() => {
     document.title = `Counter set to ${counter} | Ultimate Counter`;
     console.log("Title was set");
   }, [counter]);
 
+  useEffect(() => {
+    const savedCounterValue = localStorage.getItem("ultimateCounter");
+    if (savedCounterValue != null) {
+      setCounter(parseInt(savedCounterValue, 10));
+    }
+  }, []);
+
   const onCountClickHandler = () => {
     setCounter((c) => c + 1);
   };
 
-  const onInternalCountClickHandler = () => {
-    setInternalCount((i) => i + 1);
+  const onSaveClickHandler = () => {
+    localStorage.setItem("ultimateCounter", counter);
   };
-
-  console.log("Main render return");
 
   return (
     <Fragment>
@@ -25,8 +29,8 @@ const Counter = () => {
       <button type="button" onClick={onCountClickHandler}>
         Increment
       </button>
-      <button type="button" onClick={onInternalCountClickHandler}>
-        Increment internal count
+      <button type="button" onClick={onSaveClickHandler}>
+        Save counter value
       </button>
     </Fragment>
   );
