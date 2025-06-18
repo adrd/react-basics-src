ff150bd4ff26229e65d45ba051b3ed41776a2d72
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-18 18:08:42 +0300

    Lesson 40 - Binding multiple form controls to Component State.

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,67 @@
 import React, { useState } from "react";
 import "./form.css";
 
+// const Form = () => {
+//   console.log("component start executing");
+//   const [firstName, setFirstName] = useState("");
+
+//   const onChangeHandler = (e) => {
+//     // console.log(e);
+//     // console.log(e.target);
+//     // console.log(e.target.value);
+//     setFirstName(e.target.value);
+//   };
+
+//   return (
+//     <form>
+//       <span>{firstName}</span>
+//       <label htmlFor="firstName">First name</label>
+//       <input id="firstName" onChange={onChangeHandler} value={firstName} />
+//     </form>
+//   );
+// };
+
+// export default Form;
+
+////////////////////////////
+
 const Form = () => {
   console.log("component start executing");
-  const [firstName, setFirstName] = useState("");
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
   const onChangeHandler = (e) => {
     // console.log(e);
     // console.log(e.target);
+    // console.log(e.target.name);
     // console.log(e.target.value);
-    setFirstName(e.target.value);
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <form>
-      <span>{firstName}</span>
+      <span>{`Your name is ${formState.firstName} ${formState.lastName}`}</span>
       <label htmlFor="firstName">First name</label>
-      <input id="firstName" onChange={onChangeHandler} value={firstName} />
+      <input
+        id="firstName"
+        name="firstName"
+        onChange={onChangeHandler}
+        value={formState.firstName}
+      />
+      <label htmlFor="lastName">Last name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        onChange={onChangeHandler}
+        value={formState.lastName}
+      />
     </form>
   );
 };
+
 export default Form;
