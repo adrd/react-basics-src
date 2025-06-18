342073747932266b73d5ecabdb2fa00f89f65e3a
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-18 13:36:59 +0300

    Lesson 37 - Cleaning up after side effects.

--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,30 @@
 import { Fragment, useState, useEffect } from "react";
 
+const subscribe = (count) => {
+  console.log(`Subscribed for ${count}`);
+};
+
+const unsubscribe = (count) => {
+  console.log(`Unsubscribe for ${count}`);
+};
+
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     document.title = `Counter set to ${counter} | Ultimate Counter`;
-    console.log("Title was set");
   }, [counter]);
 
   useEffect(() => {
-    const savedCounterValue = localStorage.getItem("ultimateCounter");
-    if (savedCounterValue != null) {
-      setCounter(parseInt(savedCounterValue, 10));
-    }
-  }, []);
+    subscribe(counter);
+
+    return () => {
+      unsubscribe(counter);
+    };
+  }, [counter]);
 
   const onCountClickHandler = () => {
     setCounter((c) => c + 1);
-  };
-
-  const onSaveClickHandler = () => {
-    localStorage.setItem("ultimateCounter", counter);
   };
 
   return (
@@ -28,9 +32,6 @@ const Counter = () => {
       <p>{counter}</p>
       <button type="button" onClick={onCountClickHandler}>
         Increment
-      </button>
-      <button type="button" onClick={onSaveClickHandler}>
-        Save counter value
       </button>
     </Fragment>
   );
