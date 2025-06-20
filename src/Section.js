490e1b2626627cda42a5ee2a2ee5cd3c2301b698
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-20 15:50:39 +0300

    Lesson 65 - Making routing accessible.

--- src/Section.js
+++ src/Section.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Helmet from "react-helmet";
 
 const Section = ({ children, headingText, headingLevel = 1 }) => {
   const H = `h${headingLevel}`;
+
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    headingRef.current.focus();
+    console.log(headingRef.current);
+  }, []);
 
   return (
     <section>
       <Helmet>
         <title>{`${headingText} | Ultimate Weather Report`}</title>
       </Helmet>
-      <H>{headingText}</H>
+      <H ref={headingRef} tabIndex="-1">
+        {headingText}
+      </H>
       {children}
     </section>
   );
