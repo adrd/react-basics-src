8eddb9e1d54d2b94a450f1f723c6b312f7733f26
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-20 13:21:47 +0300

    Lesson 61 - Adding React Router to your application.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import Container from "./Container";
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./temperatures.scss";
 
-const App = () => <Container />;
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
