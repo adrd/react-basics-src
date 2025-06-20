3b187c8662db903dc34366a33b7344275139ca7a
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-20 13:53:56 +0300

    Lesson 62 - Navigating with the router's Link Component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Home from "./Home";
 import About from "./About";
+import Footer from "./Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./temperatures.scss";
 
 export const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Router>
+  <Fragment>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </Fragment>
 );
 
 export default App;
